114d972438318c35afb4dc9d90e8032049d22c6e
cdelmoral <carderomad@gmail.com>
2016-02-17 00:25:09 -0600

    Add migration for users

--- src/users/user-migrations.js
+++ src/users/user-migrations.js
@@ -1,45 +1,52 @@
 var Micropost = require('../microposts/micropost-model');
+var crypto = require('crypto');
 
-var currentSchemaVersion = 3;
+var currentSchemaVersion = 4;
 
 exports.currentSchemaVersion = currentSchemaVersion;
 exports.handleMigrations = handleMigrations;
 
-function handleMigrations(user) {
-    if (!user.schema_version) {
-        user.schema_version = 0;
-    }
+function handleMigrations(next) {
+  var user = this;
 
-    if (user.schema_version < currentSchemaVersion) {
-        upgradeSchema(user);
-    } else if (user.schema_version > currentSchemaVersion) {
-        downgradeSchema(user);
-    }
+  if (!user.schema_version) {
+    user.schema_version = 0;
+  }
+
+  if (user.schema_version < currentSchemaVersion) {
+    upgradeSchema(user);
+  } else if (user.schema_version > currentSchemaVersion) {
+    downgradeSchema(user);
+  }
+
+  user.save(next);
 }
 
 /**
  * Handles upgrade shcema migrations.
- * @param  {Object} object representing the outdated user
+ * @param {Object} object representing the outdated user
  */
 function upgradeSchema(user) {
-	// switch (user.schema_version) {
-	// 	case 0:
-	// 		user.admin = false;
-	// 	case 1:
-	// 		user.activated = true;
-	// 		user.activated_at = new Date(0);
-	// 	case 2:
-	// 		Micropost.getMicropostsCountForUser(user._id).then(function(count) {
-	// 			user.microposts_count = count;
-	// 		});
-	// }
+  switch (user.schema_version) {
+    case 0:
+      user.admin = false;
+    case 1:
+      user.activated = true;
+      user.activated_at = new Date(0);
+    case 2:
+      Micropost.getMicropostsCountForUser(user._id).then(function(count) {
+        user.microposts_count = count;
+      });
+    case 3:
+      var gravatarId = crypto.createHash('md5').update(user.email).digest("hex");
+      user.gravatar_id = gravatarId;
+  }
 
- //    user.schema_version = currentSchemaVersion;
- //    user.save();
+  user.schema_version = currentSchemaVersion;
 }
 
 /** Handles downgrade schema migrations */
 function downgradeSchema(user) {
-    // user.schema_version = currentSchemaVersion;
-    // user.save();
+  // user.schema_version = currentSchemaVersion;
+  // user.save();
 }
