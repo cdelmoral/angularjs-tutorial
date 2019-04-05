16be45adb972fb9faddf1784d43230f1bf5055bf
Carlos del Moral <carderomad@gmail.com>
2019-04-04 22:46:41 -0500

    Remove dummy test

--- test/user/user-model.test.js
+++ test/user/user-model.test.js
@@ -1,12 +1,3 @@
-var assert = require('assert');
-describe('Array', () => {
-    describe('#indexOf()', () => {
-        it('should return -1 when the value is not present', () => {
-            assert.equal([1, 2, 3].indexOf(4), -1);
-        });
-    });
-});
-
 // var User = require('../../src/users/user-model');
 // var should = require('should');
 // var validator = require('validator');
